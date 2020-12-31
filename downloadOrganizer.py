9544482ec734ebaa532be89fb47be7fd9cabe0d0
jeffdeng1314 <32289287+jeffdeng1314@users.noreply.github.com>
2020-12-30 17:01:35 -0800

    music to itunes. Included more attributes for folder organizer

--- downloadOrganizer.py
+++ downloadOrganizer.py
@@ -4,20 +4,33 @@ from pathlib import Path
 # https://www.geeksforgeeks.org/junk-file-organizer-python/
 DIRECTORIES = {
     "HTML": [".html5", ".html", ".htm", ".xhtml"],
-    "IMAGES": [".jpeg", ".jpg", ".tiff", ".gif", ".bmp", ".png", ".bpg", "svg",
+
+    "Images": [".jpeg", ".jpg", ".tiff", ".gif", ".bmp", ".png", ".bpg", "svg",
                ".heif", ".psd"],
-    "VIDEOS": [".avi", ".flv", ".wmv", ".mov", ".mp4", ".webm", ".vob", ".mng",
+
+    "Videos": [".avi", ".flv", ".wmv", ".mov", ".mp4", ".webm", ".vob", ".mng",
                ".qt", ".mpg", ".mpeg", ".3gp"],
-    "DOCUMENTS": [".oxps", ".csv", ".pages", ".docx", ".doc", ".fdf", ".ods",
-                  ".odt", ".pwi", ".xsn", ".xps", ".dotx", ".docm", ".dox",
-                  ".rvg", ".rtf", ".rtfd", ".wpd", ".xls", ".xlsx", ".ppt",
-                  ".pptx"],
-    "ARCHIVES": [".a", ".ar", ".cpio", ".iso", ".tar", ".gz", ".rz", ".7z",
-                 ".dmg", ".rar", ".xar", ".zip"],
-    "AUDIO": [".aac", ".aa", ".aac", ".dvf", ".m4a", ".m4b", ".m4p",
-              ".msv", "ogg", "oga", ".raw", ".vox", ".wav", ".wma"],
-    "MUSIC": [".mp3"],
-    "PLAINTEXT": [".txt", ".in", ".out"],
+
+    "Documents": [".oxps", ".pages", ".fdf", ".ods",
+                  ".odt", ".pwi", ".xsn", ".xps",
+                  ".rvg", ".rtf", ".rtfd", ".wpd"],
+
+    "Microsoft Documents": [".xls", ".xlsx", ".ppt",
+                  ".pptx", ".csv",".docx", ".doc", ".dotx", ".docm", ".dox" ],
+
+    "Archives": [".a", ".ar", ".cpio", ".iso",
+                 ".dmg", ".rar", ".xar"],
+    
+    "Zip Files": [".tar", ".gz", ".rz", ".7z",
+                  ".zip"],
+
+    "Audio": [".aac", ".aa", ".dvf", ".m4a", ".m4b", ".m4p",
+              ".msv", "ogg", "oga", ".raw", ".vox", ".wav", ".wma", ".flac"],
+
+    "Music": [".mp3"],
+    "MATLAB": [".m"],
+    "Icons": [".ico"],
+    "Plain Text": [".txt", ".in", ".out"],
     "PDF": [".pdf"],
     "XML": [".xml"],
     "EXE": [".exe"],
@@ -29,13 +42,20 @@ FILE_FORMATS = {file_format: directory
                 for directory, file_formats in DIRECTORIES.items() 
                 for file_format in file_formats} 
 
+class downloadFolderPath:
 
-def organizer():
     username = os.environ['USERNAME']
-
     downloadPath = f"C:\\Users\\{username}\\Downloads"
 
-    for entry in os.scandir(downloadPath):
+    def __init__(self):
+        pass
+
+    
+
+
+def organizer():
+
+    for entry in os.scandir(downloadFolderPath.downloadPath):
         if entry.is_dir():
             continue
 
@@ -43,7 +63,7 @@ def organizer():
         # print(entry.path)
 
         if file_path in FILE_FORMATS:
-            dir_path = Path(f"{downloadPath}\\{FILE_FORMATS[file_path]}")
+            dir_path = Path(f"{downloadFolderPath.downloadPath}\\{FILE_FORMATS[file_path]}")
 
             # FileExistsError exception will be ignored
             dir_path.mkdir(exist_ok=True)
@@ -52,14 +72,11 @@ def organizer():
             os.system(f'move \"{entry.path}\" {dir_path}')
 
 
-def generate():
-    username = os.environ['USERNAME']
+# def generate():
 
-    downloadPath = f"C:\\Users\\{username}\\Downloads"
-
-    for i in range(20):
-        f = "testing" + str(i)
-        os.system(f'touch {downloadPath}\\{f}.txt')
+#     for i in range(20):
+#         f = "testing" + str(i)
+#         os.system(f'touch {downloadFolderPath.downloadPath}\\{f}.txt')
 
 
 if __name__ == "__main__":
