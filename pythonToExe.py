7ee95110cd3dda05b36cc42476d478ae59bacf83
jeffdeng1314 <32289287+jeffdeng1314@users.noreply.github.com>
2021-01-22 18:10:24 -0800

    renamed the files

--- pythonToExe.py
+++ pythonToExe.py
@@ -2,14 +2,13 @@ from __future__ import unicode_literals
 from distutils.core import setup
 import py2exe, sys, os
 from pathlib import Path
-import youtube_dl
-import eyed3
+
 
 sys.argv.append('py2exe')
 
 setup(
     options = {'py2exe': {'bundle_files': 1, 'compressed': True}},
     # windows = [{'script': "youtubeAudioConverter.py"}],
-    console = [{"script" :"youtubeAudioConverter.py"}], #for stdin, replacing windows with console for input
+    console = [{"script" :"Photo\ Downloader.py"}], #for stdin, replacing windows with console for input
     zipfile = None,
 )
