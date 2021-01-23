dc0f1db3ad45c0e375e2135e9395306817cb56a7
jeffdeng1314 <32289287+jeffdeng1314@users.noreply.github.com>
2021-01-22 19:28:13 -0800

    yeah

--- pythonToExe.py
+++ pythonToExe.py
@@ -1,14 +1,18 @@
-from __future__ import unicode_literals
-from distutils.core import setup
-import py2exe, sys, os
-from pathlib import Path
+# from __future__ import unicode_literals
+# from distutils.core import setup
+# import py2exe, sys, os
+# from pathlib import Path
 
 
-sys.argv.append('py2exe')
+# sys.argv.append('py2exe')
 
-setup(
-    options = {'py2exe': {'bundle_files': 1, 'compressed': True}},
-    # windows = [{'script': "youtubeAudioConverter.py"}],
-    console = [{"script" :"Photo\ Downloader.py"}], #for stdin, replacing windows with console for input
-    zipfile = None,
-)
+# setup(
+#     options = {'py2exe': {'bundle_files': 1, 'compressed': True}},
+#     # windows = [{'script': "youtubeAudioConverter.py"}],
+#     console = [{"script" :"DL_Folder_Organizer.py"}], #for stdin, replacing windows with console for input
+#     zipfile = None,
+# )
+
+
+# apparently, Py2exe stopped develop at Python 3.4 and do not work for newer versions.
+# Use pyinstaller work Python 3.7.
