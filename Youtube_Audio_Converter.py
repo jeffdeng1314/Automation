d67ff195d6f74350ad4906681759088d1fdae010
jeffdeng1314 <32289287+jeffdeng1314@users.noreply.github.com>
2021-05-07 01:20:56 -0700

    added the avoid_characters list to making sure file with the correct name when downloaded

--- Youtube_Audio_Converter.py
+++ Youtube_Audio_Converter.py
@@ -12,6 +12,21 @@ INDICATORS = {
             '【' : '】'
         }
 
+AVOID_CHARACTERS = {
+    # \ / : * ? " < > |
+    # A file cannot contain these characters
+
+    '\\' : '',
+    '/' : '_',
+    ':' : '',
+    '*' : '',
+    '?' : '',
+    '"' : "'",
+    '<' : '',
+    '>' : '',
+    '|' : '_'
+}
+
 # Downloads to the Download folder 
 def youtubeAudioConverter():
 
@@ -41,41 +56,50 @@ def youtubeAudioConverter():
                 continue
             
             print("trackTitle: ", trackTitle)
-            newTrackTitle = trackTitle = trackTitle.replace('/','_')
+
+            for c in trackTitle:
+                if c in AVOID_CHARACTERS:
+                    trackTitle = trackTitle.replace(c,AVOID_CHARACTERS[c])
+
+            
             print('After replacement with _: ' + trackTitle)
-                
+
+            newTrackTitle = trackTitle    
             # looping through the trackk title to remove anything from INDICATORS
             # Assuming the title won't contain [()] or ([]) or any of these combinations 
             start = end = -1
             for i in range(len(trackTitle)):
-                if trackTitle[i] in INDICATORS:
-                    start = i
-                    continue
-
                 # Make sure we can detect an indicator first
                 if start >= 0:
                     if trackTitle[i] == INDICATORS[trackTitle[start]]:
                         end = i
 
                         # remove the white space before [ or (, it's kinda a cheat way
+                        print(f'Start: {start}, End: {end}')
                         newTrackTitle = trackTitle[:start-1] + trackTitle[end+1:]
                         break
+
+                if trackTitle[i] in INDICATORS:
+                    start = i
+                    continue
+
+
             print('!!!newTrackTitle: ', newTrackTitle)
-            
+            print('**trackTtile: ',trackTitle)
             # To songs like artist - song title [lyrics]??
             if '-' in newTrackTitle:
                 artist = newTrackTitle.split('-')[0]
-                
                 # [1:] to remove the single white space in the front
                 title = newTrackTitle.split('-')[1][1:]
 
-                loadFile = savePath + "/" + trackTitle + ".mp3"
+                # loadFile = savePath + "/" + trackTitle + ".mp3"
+                loadFile = savePath + '/' + trackTitle + ".mp3"
+                print("LoadFile: ",loadFile)
                 e = eyed3.load(loadFile)
                 e.tag.artist = artist
                 e.tag.title = title
                 e.rename(title)
                 e.tag.save()
-
 
 
 
